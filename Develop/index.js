4e27121652dc73820c7bbd89d150de774b8e58c5
Molly Wobbles <molly.rendon@outlook.com>
2021-12-14 14:54:56 -0500

    Added license functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,47 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name:  'Description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions? Dependencies, commands that are needed.',
+        name:  'Installation',
+    },
+    {
+        type: 'input',
+        message: 'How do you use this application?',
+        name:  'Usage',
+    },
+    {
+        type: 'checkbox',
+        message: 'Choose from the following licenses.',
+        name:  'License',
+    },
+    {
+        type: 'input',
+        message: 'Who are the contributors for this project?',
+        name:  'Contributing',
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name:  'userName',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name:  'Email',
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
